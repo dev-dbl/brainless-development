1e21af9c58812448581766edd13d19b09ef1603f
dev-dbl <development.dbl@gmail.com>
2021-07-31 14:54:12 +0200

    init

---
+++ apps/delivery-parcel-printer/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './shared/shell/shell.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { OrdersListComponent } from './pages/orders/orders-list/orders-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    // canActivate: [AuthGuard],
+    component: ShellComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      // {
+      //   path: 'orders',
+      //   component: OrdersListComponent
+      // }
+    ]
+  },
+  // {
+  //   path: '**',
+  //   redirectTo: '',
+  //   pathMatch: 'full'
+  // }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- apps/delivery-parcel-printer/src/app/app.module.ts
+++ apps/delivery-parcel-printer/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ShellComponent, DashboardComponent],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
