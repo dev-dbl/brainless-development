1a52c95b1dfa5d453bcaafabf0b492b7be69d98c
dev-dbl <development.dbl@gmail.com>
2021-07-31 15:37:16 +0200

    orders lib

--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OrdersListComponent } from './pages/orders-list/orders-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    OrdersListComponent
+  ]
 })
 export class OrdersModule {}
