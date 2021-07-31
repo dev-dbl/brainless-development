1c145c901c147dccc0535aee1a4c4dad066ee058
dev-dbl <development.dbl@gmail.com>
2021-07-31 14:33:23 +0200

    initial commit

---
+++ apps/delivery-parcel-printer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'brainless-development-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'delivery-parcel-printer';
+}
